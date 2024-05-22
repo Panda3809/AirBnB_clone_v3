35a687366f170cceac8daa4864268d3fabc9c6d7
Akubada Daniel <dasuseyi@gmail.com>
2024-05-22 16:04:09 +0000

    run web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
-from flask import Flask
+#!/usr/bin/python3
+"""The / route"""
 
-app = Flask(__name__)
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    from flask import Flask
 
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def hello_route():
+        """root folder route"""
+        return "Hello HBNB!"
+
+    app.run(host='0.0.0.0')
